974a7a6d7f0de7f608d9c09eac429ceef0b798ea
kromchenko <kromchenko1987@gmail.com>
2019-05-11 12:22:46 +0300

    add project

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
   <div id="app">
-    
+    <Navigation></Navigation>
+    <Teaser></Teaser>
+    <CharactersList></CharactersList>
   </div>
 </template>
 
 <script>
-import Navigation from './components/Navigation.vue';
-import Teaser from './components/Teaser.vue';
-import CharactersList from './components/CharactersList.vue';
-
-export default {
-  name: 'app',
-  components: {
-    Navigation, Teaser, CharactersList
+  import Navigation from './components/Navigation.vue';
+  import Teaser from './components/Teaser.vue';
+  import CharactersList from './components/CharactersList.vue';
+  import style from './asserts/style.css';
+  export default {
+    name: 'app',
+    components: {
+      Navigation, Teaser, CharactersList
+    }
   }
-}
 </script>
 
 <style>
-
 </style>
